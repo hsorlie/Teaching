17a5c6213ff21d5b52e7c8e4b77ae6b6904b41fa
Henrik Sørlie <hsorlie@icloud.com>
2019-10-15 22:27:27 +0200

    Shiny-appen virker!

--- Demo-Sentralgrenseteoremet/app.R
+++ Demo-Sentralgrenseteoremet/app.R
@@ -17,46 +17,45 @@ ui <- fluidPage(
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
-        sidebarPanel(
-            sliderInput("terninger",
-                        "Antall Terninger:",
+            sliderInput(inputId = "terninger",
+                        label = "Antall Terninger:",
                         min = 1,
-                        max = 20,
-                        value = 1)
-        ),
-        sidebarPanel(
-            sliderInput("repetisjoner",
-                        "Antall ganger:",
+                        max = 10,
+                        value = 1),
+            sliderInput(inputId = "repetisjoner",
+                        label = "Antall ganger:",
                         min = 1,
-                        max = 100,
+                        max = 10000,
                         value = 1)
-        ),
+            ),
 
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("plot")
+           plotOutput(outputId = "plot")
         )
     )
-)
+
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    resultat <- integer(0)
-    tabell <- tibble(
-        verdi = terninger:(6*terninger),
-        antall = 0)
-    
-    # Gjenta det følgende så mange ganger som angitt
-    for(i in 1:repetisjoner){
-        # Kast antallet terninger som er angitt
-        kast <- sample(1:6, size = terninger, replace = TRUE)
-        # Legg summen av terningkastet til resultatet
-        resultat <- c(resultat, sum(kast))
-        tabell$antall[sum(kast)-(terninger-1)] <- tabell$antall[sum(kast)-(terninger-1)]+1
-    }
-    
-        output$plot <- ggplot(data = tabell) + 
+    output$plot <- renderPlot({
+        
+        resultat <- integer(0)
+        tabell <- tibble(
+            verdi = input$terninger:(6*input$terninger),
+            antall = 0)
+            
+# Gjenta det følgende så mange ganger som angitt
+            for(i in 1:input$repetisjoner){
+                # Kast antallet terninger som er angitt
+                kast <- sample(1:6, size = input$terninger, replace = TRUE)
+                # Legg summen av terningkastet til resultatet
+                resultat <- c(resultat, sum(kast))
+                tabell$antall[sum(kast)-(input$terninger-1)] <- tabell$antall[sum(kast)-(input$terninger-1)]+1
+            }
+            
+            
+            ggplot(data = tabell) + 
             geom_col(
                 mapping = aes(
                     x = verdi,
@@ -65,12 +64,22 @@ server <- function(input, output) {
                     fill = I("cornflowerblue"))) +
             scale_x_discrete(
                 name = "Verdi pr. terningkast",
-                limits = as.character(1:(6*terninger)),
-                breaks = as.character(terninger:(6*terninger)),
-                labels = as.character(terninger:(6*terninger))) +
-            scale_y_continuous(name = "Antall kast")
-
+                limits = as.character(1:(6*input$terninger)),
+                breaks = as.character(input$terninger:(6*input$terninger)),
+                labels = as.character(input$terninger:(6*input$terninger))) +
+            scale_y_continuous(name = "Antall kast") +
+                # Legg til normalfordelingskurve
+                if(input$terninger > 1 & input$repetisjoner > 10){
+                    stat_function( 
+                        fun = function(x, mean, sd){ 
+                            dnorm(
+                                x = x,
+                                mean = mean(tabell$verdi),
+                                sd = sd(resultat)) * length(resultat)
+                        }, 
+                        args = c(mean = mean, sd = sd))
+                }
+})
 }
-
 # Run the application 
 shinyApp(ui = ui, server = server)
