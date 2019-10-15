f946f2080b8573e452b53f339406403daacfd54d
Henrik Sørlie <hsorlie@icloud.com>
2019-10-15 23:24:09 +0200

    Virker på shiny server

--- Demo-Sentralgrenseteoremet/app.R
+++ Demo-Sentralgrenseteoremet/app.R
@@ -1,42 +1,33 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(tibble)
+library(ggplot2)
 
-# Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
     titlePanel("Demo Sentralgrenseteoremet"),
 
-    # Sidebar with a slider input for number of bins 
+
     sidebarLayout(
+        sidebarPanel(
             sliderInput(inputId = "terninger",
-                        label = "Antall Terninger:",
+                        label = "Antall terninger pr. kast:",
                         min = 1,
                         max = 10,
                         value = 1),
             sliderInput(inputId = "repetisjoner",
-                        label = "Antall ganger:",
+                        label = "Antall kast:",
                         min = 1,
                         max = 10000,
                         value = 1)
             ),
 
-        # Show a plot of the generated distribution
         mainPanel(
            plotOutput(outputId = "plot")
-        )
+        ))
     )
 
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
     output$plot <- renderPlot({
         
@@ -81,5 +72,6 @@ server <- function(input, output) {
                 }
 })
 }
+
 # Run the application 
 shinyApp(ui = ui, server = server)
