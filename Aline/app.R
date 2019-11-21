7f72e5a8f45baab8ce8819a2accad75617a23107
Henrik Sørlie <hsorlie@icloud.com>
2019-11-21 23:13:40 +0100

    Demo: A Straight Line (Shiny)

---
+++ Aline/app.R
@@ -0,0 +1,66 @@
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("A Straight Line"),
+
+    sidebarLayout(
+        sidebarPanel(
+            includeText("top.txt"),hr(),
+            sliderInput(inputId = "intercept",
+                        label = "intercept: a",
+                        min = -2,
+                        max = 2,
+                        value = 0.5,
+                        step = 0.1),
+            sliderInput(inputId = "slope",
+                        label = "slope: b",
+                        min = -2,
+                        max = 2,
+                        value = 0.5,
+                        step = 0.1),
+        ),
+        
+        mainPanel(
+            plotOutput(outputId = "plot")
+        )),hr(),includeMarkdown("explanation.md"),hr(),includeMarkdown("footer.md")
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+    observeEvent(input$update, {
+
+    output$plot <- renderPlot({
+
+        data <- tibble(
+            x = seq(from = -3, to = 3, by = 0.1),
+            y = input$intercept + input$slope * x
+        )
+                
+        ggplot(data = data) + 
+            geom_line(
+                mapping = aes(
+                    x = x,
+                    y = y,
+                    colour = I("blue2"))) +
+            scale_x_continuous(
+                name = "x",
+                limits = c(-2,2),
+                breaks = c(-2, -1, 0, 1, 2)) +
+            scale_y_continuous(
+                name = "y",
+                limits = c(-2,2),
+                breaks = c(-2, -1, 0, 1, 2)) +
+            geom_vline(aes(xintercept = 0), show.legend = FALSE) +
+            geom_hline(aes(yintercept = 0), show.legend = FALSE)
+            
+            }
+)
+    }, ignoreNULL = FALSE)   
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
---
+++ Aline/explanation.md
@@ -0,0 +1,5 @@
+---
+title: ""
+output: html_document
+---
+A straight line on a plane is defined by two parameters: the intercept (a) and the slope (b). By setting these two parameters, any straight line can be described. The intercept determines where the slope intercepts the y-axis, hence the name. The slope determines... well, the slope of the line. That is, for every increment along the x-axis, how much will the corresponding value on the y-axis increase.
